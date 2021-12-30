d3a5156c55c29e63c1f8ae9015c4444f76525439
manuelpt49 <manuelpt4940@gmail.com>
2021-12-30 14:12:15 -0500

    Configuring panel of control, adding privilegies to users to view, add, edit and delete data

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes, Articulos, Pedidos
+
 # Register your models here.
+
+class ClientesAdmin (admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "direccion", "telefono")
+    search_fields = ("nombre", "apellido", "direccion")
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ["seccion"]
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha", "entregado")
+    list_filter = ["fecha"]
+    date_hierarchy = "fecha"
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -7,8 +7,11 @@ class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return 'El cliente se llama %s %s, su direccion física es %s y la de correo es %s. Su contacto es %s' % (self.nombre, self.apellido, self.direccion, self.email, self.telefono)
 
 
 
@@ -26,3 +29,9 @@ class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        if self.entregado:
+            return 'El pedido %s fue de %s y su estado es entregado' % (self.numero, self.fecha)
+        else:
+            return 'El pedido %s fue de %s y su estado es no entregado' % (self.numero, self.fecha)
