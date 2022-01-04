b266635ea135cbc5d1548084c49b24a9fc1e2606
manuelpt49 <manuelpt4940@gmail.com>
2022-01-04 12:38:46 -0500

    Adding post in blog section. Making logic to identify unique categories of post into Post View and pass it throug dict to template. Staring video 41

--- ProyectoWeb/BlogApp/views.py
+++ ProyectoWeb/BlogApp/views.py
@@ -4,4 +4,9 @@ from BlogApp.models import Post
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
-    return render(request, "blog.html", {'posts': posts})
+    unique_categorias = []
+    for post in posts:
+        for categoria in post.categorias.all():
+            unique_categorias.append(categoria.nombre)
+    unique_categorias = set(unique_categorias)
+    return render(request, "blog.html", {'posts': posts, 'categorias': unique_categorias})
