be9490cdbf3022b140df59ea6222f3ba239a4539
manuelpt49 <manuelpt4940@gmail.com>
2022-01-05 09:11:38 -0600

    ImpÂroving blog section

--- ProyectoWeb/BlogApp/views.py
+++ ProyectoWeb/BlogApp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from BlogApp.models import Post
+from BlogApp.models import Categoria, Post
 
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
+    unique_categorias = obtaining_categories(posts)
+    return render(request, "blog.html", {'posts': posts, 'categorias': unique_categorias})
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id = categoria_id)
+    posts = Post.objects.filter(categorias = categoria)
+    return render(request, "categoria.html", {'categoria': categoria, 'posts':posts})
+
+
+def obtaining_categories(posts):
+    """Class to obtain categories according to posts available in webpage"""
     unique_categorias = []
     for post in posts:
         for categoria in post.categorias.all():
-            unique_categorias.append(categoria.nombre)
+            #unique_categorias.append({categoria.nombre, categoria.id})
+            unique_categorias.append(categoria)
     unique_categorias = set(unique_categorias)
-    return render(request, "blog.html", {'posts': posts, 'categorias': unique_categorias})
+    return unique_categorias
